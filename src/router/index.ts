f8255a7de9861c75f65d7c92a8f32efd5504e066
lovedaxigua <1429108957@qq.com>
2021-05-05 13:43:52 +0800

    first-commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/Avout.vue'
+import About from '../views/About.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
